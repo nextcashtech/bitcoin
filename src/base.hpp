b65dd24a112acad224b515ec00e3383050acb4af
Curtis Ellis <curtis@arcmist.com>
2017-10-23 14:42:30 -0600

    Update user agent version to 0.4.0

--- src/base.hpp
+++ src/base.hpp
@@ -14,7 +14,7 @@
 #include <cstdint>
 #include <ctime>
 
-#define BITCOIN_USER_AGENT "/ArcMist:0.3.0/" // BIP-0014
+#define BITCOIN_USER_AGENT "/ArcMist:0.4.0/" // BIP-0014 Specifies User Agent Format
 #define PROTOCOL_VERSION 70015
 
 
