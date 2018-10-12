8e5af11c9177cb9dd4ee17ff536226535891ead8
curtis <curtis@nextcash.tech>
2018-10-12 08:35:43 -0600

    Increment Android version to 0.9.1

--- src/base.hpp
+++ src/base.hpp
@@ -17,7 +17,7 @@
 
 // BIP-0014 Specifies User Agent Format
 #ifdef ANDROID
-#define BITCOIN_USER_AGENT "/NextCash:0.11.0/NextCashWallet:0.8.2(Android)/"
+#define BITCOIN_USER_AGENT "/NextCash:0.11.0/NextCashWallet:0.9.1(Android)/"
 #else
 #define BITCOIN_USER_AGENT "/NextCash:0.11.0/"
 #endif
